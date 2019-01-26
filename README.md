b8594f3d811fe14f17ecf6a46e1883ade472a448
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2019-01-26 12:53:51 -0600

    Install dependencies
    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TAMUhack-2019
+
+Challenges we're going for
+  - Best use of Google Cloud Platform
+  - Best use of the Smartcar API
+  - Best domain name registered with domain.com
+  
---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+
+setup(
+	name="tamuhackers19",
+	version="1.0.0",
+	description="Our source code for TAMUhack 2019",
+	packages=find_packages(),
+	include_package_data=True,
+	zip_safe=False,
+)
