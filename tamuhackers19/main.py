d49c7b630bf9502a0e610c904f3d87a2c4d30c9e
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2019-01-26 16:15:43 -0600

    Update base entrypoint and add cloudbuild.yml

--- tamuhackers19/main.py
+++ tamuhackers19/main.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import os
+
+from flask import Flask, render_template, redirect
+
+import smartcar
+
+app = Flask("tamuhack19")
+
+@app.route("/")
+def home():
+    return "Base template built successfully"
+    
 if __name__ == "__main__":
-    print("Hello World")
+    host = "0.0.0.0"
+    port = 5000
+    print(f"Initializing application on {host}:{port} ")
+    app.run(host=host, port=port)
