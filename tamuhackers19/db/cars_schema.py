c4c1db216bb72d84166abebe9a13f535429b84c6
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2019-01-26 17:44:19 -0600

    Include registration class code in car schema

--- tamuhackers19/db/cars_schema.py
+++ tamuhackers19/db/cars_schema.py
@@ -4,10 +4,22 @@ from marshmallow import Schema, fields
 from marshmallow.exceptions import ValidationError
 
 class CarsSchema(Schema):
+    """
+    Notes
+    -----
+    registration_class_code : str May be any of
+      - PAS (Passenge Vehicles)
+      - SRF (Personalized Plates)
+      - COM (Commercial Vechicles)
+      - MOT (Motorcycles)
+      - HSM (Historical Motorcycles)
+      - LMA, LMB, LMC (Limited Use Motorcycles)
+    """
     model = fields.String()
     make = fields.String()
     year = fields.Integer()
     vin = fields.String()
+    registration_class_code = fields.String()
     license_plate = fields.String()
 
 
@@ -16,10 +28,12 @@ if __name__ == "__main__":
         "model": "Model 3",
         "make": "Tesla",
         "year": 2019,
+        "registration_class_code": "PAS",
         "vin": "1N4BA41E94C895344",
         "license_plate": "6JTR1E",
     }
     try:
         data, error = CarsSchema(strict=True).load(data)
+        print(data)
     except ValidationError as e:
         print(e)
