1d785de1896b868de9ba7ae0c946f0185cec3ccb
Victoria Gomez <gmzvic07@gmail.com>
2019-01-26 18:15:41 -0600

    added stuff for smartcar

---
+++ tamuhackers19/carInfo.py
@@ -0,0 +1,68 @@
+# ./main.py
+
+#can create variables for client id and client secret as strings
+import os
+import smartcar 
+from flask import Flask, redirect, request, jsonify
+app=Flask("myapp")
+
+vehicle_url = "http://localhost:8000/vehicle"
+# TODO: Authorization Step 1a: Launch Smartcar authentication dialog
+client = smartcar.AuthClient(
+    client_id=os.environ.get('CLIENT_ID'),
+    client_secret=os.environ.get('CLIENT_SECRET'),
+    redirect_uri=os.environ.get('REDIRECT_URI'),
+    scope=['read_vehicle_info'],
+    test_mode=True,
+)
+
+@app.route('/login', methods=['GET'])
+
+#def index
+def login():
+    # TODO: Authorization Step 1b: Launch Smartcar authentication dialog
+    auth_url = client.get_auth_url()
+    return redirect(auth_url)
+
+access = None
+@app.route('/exchange', methods=['GET'])
+# global variable to save our access_token
+
+def exchange():
+    code = request.args.get('code')
+
+    # TODO: Request Step 1: Obtain an access token
+    # access our global variable and store our access tokens
+    global access
+    # in a production app you'll want to store this in some kind of
+    # persistent storage
+    access = client.exchange_code(code)
+    return redirect(vehicle_url)
+
+@app.route('/vehicle', methods=['GET'])
+def vehicle():
+    # access our global variable to retrieve our access tokens
+    global access
+    # the list of vehicle ids
+    vehicle_ids = smartcar.get_vehicle_ids(
+        access['access_token'])['vehicles']
+    
+    # TODO: Request Step 3: Create a vehicle    
+    # instantiate the first vehicle in the vehicle id list
+    vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
+    info = vehicle.info()
+    print(info)
+    '''
+    {
+        "id": "36ab27d0-fd9d-4455-823a-ce30af709ffc",
+        "make": "TESLA",
+        "model": "Model S",
+        "year": 2014
+    }
+    '''
+
+    return jsonify(info)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8000)
