d0bc076a848f4f8dcafac7d1ed0c61ebd15ca700
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2019-01-27 06:28:10 -0600

    Add user schema and email function

--- tamuhackers19/utils.py
+++ tamuhackers19/utils.py
@@ -1,6 +1,10 @@
 
 import os
 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 from twilio.rest import Client
 from tamuhackers19.db.firestore_client import FirestoreConnector
 
@@ -13,6 +17,32 @@ def send_text_message(message: str) -> None:
 		from_=os.environ.get("TWILIO_NUMBER"),
 		body=message,
 	)
+
+def send_email(email: str, data: str) -> None:
+	"""Send an email to the prospective customer.
+	Parameters
+	----------
+	email : str
+		The email that was submitted.
+	data : str
+		The data of the email to be sent.
+	""" 
+	body = data
+	from_address = os.environ.get("EMAIL_USER")
+	try:
+		msg = MIMEMultipart()
+		msg["From"] = from_address
+		msg["To"] = email
+		msg["Subject"] = "ReportACrash - Incident Report"
+		msg.attach(MIMEText(body, "plain"))
+		server = smtplib.SMTP("smtp.gmail.com", 587)
+		server.starttls()
+		server.login(from_address, os.environ.get("EMAIL_PASS"))
+		server.sendmail(from_address, [from_address, email], msg.as_string())
+		server.quit()
+	except Exception as e:
+		pass
+
 
 if __name__ == "__main__":
     dbc = FirestoreConnector()
